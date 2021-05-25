d55c3beb6ec6376db079e4e341f57db9a3cb5bcd
Neon Riot <sbrookshire99@gmail.com>
2021-05-25 14:05:35 -0500

    Improved security, added dotenv, cleaned up packages

--- employee_manager.js
+++ employee_manager.js
@@ -1,12 +1,13 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const dotenv = require("dotenv").config();
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Badhank6!",
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: "employee_managerdb",
 });
 
